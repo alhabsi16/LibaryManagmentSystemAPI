40696f7a8864f972a007040945e4f11fe0efdc0e
alhabsi16 <mxmx8283@gmail.com>
2023-10-10 10:42:30 +0400

    Login user

---
+++ LibraryDbContext.cs
@@ -0,0 +1,19 @@
+﻿using LibaryManagmentSystemAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibaryManagmentSystemAPI
+{
+    public class LibraryDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+
+            options.UseSqlServer("Data Source=(local);Initial Catalog=WebApi; Integrated Security=true; TrustServerCertificate=True");
+        }
+        public DbSet<Book> books { get; set; }
+        public DbSet<BorrowingTransaction> borrowingTransactions { get; set;}
+        public DbSet<Patron> patrons { get; set; }
+        public DbSet<User> users { get; set; }
+        public IEnumerable<object> Patron { get; internal set; }
+    }
+}
